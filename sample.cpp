cc35023fe26866a948065340849598d24e55eb48
rasmiranjansamantray <42238972+rasmiranjansamantray@users.noreply.github.com>
2018-08-09 16:51:49 +0530

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,7 @@
+#include<iost ream>// library
+using namespace std;//Resolve multyple definition issue of same name function.
+
+int main(){
+           cout<<"hello";//Prints to terminal
+           return 10;
+}
